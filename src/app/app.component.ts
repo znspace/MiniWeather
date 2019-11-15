823f5d577af202572d39277cdcbab534175b4888
znspace <zhounan.dev@gmail.com>
2019-11-15 17:48:03 +0800

    add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UiService } from './services/ui/ui.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'MiniWeather';
+  showMenu = false;
+  darkModeActive: boolean;
+
+  constructor(public ui: UiService) {}
+  sub1;
+
+  ngOnInit() {
+    this.sub1 = this.ui.darkModeState.subscribe(value => {
+      this.darkModeActive = value;
+    });
+  }
+
+  ngOnDestroy() {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
 }
